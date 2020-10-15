40278bccf063ee4d4b1e8b2fdabaf5a79b1d88cf
Ugur Kardas <ugur.kardas@outlook.com>
2020-10-15 14:35:06 +0200

    Created test database

---
+++ database/source.sql
@@ -0,0 +1,18 @@
+CREATE TABLE types
+(
+    fixed_size_10		char(10),
+    variable_size_10	varchar(10),
+    trö_or_falshe		boolean,
+    binary_data			blob,
+    std_int				int,
+    smol_int			smallint,
+    tall_int			bigint,
+    monay				decimal(5, 2),
+    stonks				numeric(5, 2),
+    floating			float(9),
+    better_float		real,
+    double_better		double,
+    earth_day			date,
+    moon_time			time,
+    mars_time			timestamp
+);
