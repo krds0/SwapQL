71047f3c7a55f3e012af43687e417294bf1a51ee
Kathrin Schrenk <schrenk.k01@htlwienwest.at>
2020-10-15 16:21:05 +0200

    Klassenbibliotheken für Mysql und share functions, tested connection to mysql database, classes not used ideally!

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Data.Common;
 using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using SwapQL.Config;
+using Shared_DB_Functions;
+using MySql.Data.MySqlClient;
+using MYSQL_DB_Functions;
 
 namespace SwapQL
 {
@@ -16,8 +20,9 @@ namespace SwapQL
         static async Task Main(string[] args)
         {
             Console.Title = "SwapQL";
-
+            /*
             #region Read config
+            
             Console.WriteLine("Reading configuration...");
             try
             {
@@ -32,8 +37,14 @@ namespace SwapQL
             Console.WriteLine("Configuration read...");
             Console.WriteLine($"Using options:\n\thost = {Configuration.Host}:{Configuration.Port}\n\tuser = {Configuration.User}\n\tpassword = {Configuration.Password}");
             #endregion
+            */
 
             // TODO: connenct to database
+            MySqlClientFactory test = (MySqlClientFactory)SharedFunctions.GetProvFact(SharedFunctions.DatabaseKind.Mysql);
+            Console.WriteLine("Created Mysql Provider Factory");
+
+            Console.WriteLine(MYSQLReader.ReadData("Select * from test_table;"));
+            Console.WriteLine("Successfully read from MySQL Database.");
 
             Console.Read();
         }
